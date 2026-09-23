a0f3a95656b36a9146df4161ffdf1d2f6fc542d5
Maiesha Moohan <maieshamoohan@gmail.com>
2026-09-23 11:09:44 +0200

    Saving my progress

--- Frontend/src/views/DiscoverView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <!-- DiscoverView.vue, placeholder thing so that the other pages don't crash cause PostCard.vue was edited -->
-    <h1>Discover Page</h1>
-  </div>
-</template>
